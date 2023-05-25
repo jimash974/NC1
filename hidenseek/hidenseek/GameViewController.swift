c5d3aa5de5f4d2a53fedd2945b2a77331ccc5d88
Jeremy Christopher <jeremynchristopher@gmail.com>
2023-05-25 10:52:09 +0700

    code tidying

--- hidenseek/hidenseek/GameViewController.swift
+++ hidenseek/hidenseek/GameViewController.swift
@@ -26,19 +26,13 @@ class GameViewController: UIViewController {
 //    ghost node
     var ghost:SCNNode!
     
-    //    ray node
-        var ray:SCNNode!
-    
-    //    ray2 node
-        var ray2:SCNNode!
-    
     var score:Int = 0
     
     var ghostScary: SCNNode!
     
     var sounds:[String:SCNAudioSource] = [:]
-
-
+    
+    var health:Int = 3
     
     private var scoreLabel: UILabel!
     private var lifeLabel: UILabel!
@@ -48,7 +42,6 @@ class GameViewController: UIViewController {
         setUpScene()
         setUpNodes()
         setupSounds()
-//        updateSpotlightPosition()
         sceneView.delegate = self
 //        scene.physicsWorld.contactDelegate = self
 
@@ -76,7 +69,7 @@ class GameViewController: UIViewController {
         
         scene.physicsWorld.contactDelegate = self
         
-        sceneView.debugOptions = [.showPhysicsShapes]
+//        sceneView.debugOptions = [.showPhysicsShapes]
         
         // Create the score label
         scoreLabel = UILabel(frame: CGRect(x: view.bounds.width - 100, y: 20, width: 80, height: 30))
@@ -100,7 +93,7 @@ class GameViewController: UIViewController {
 //
 //
 //        lifeLabel.text = "Your life : \(life)"
-        scoreLabel.text = "\(score)/10"
+        scoreLabel.text = "\(score)/5"
 
 
     }
@@ -109,28 +102,27 @@ class GameViewController: UIViewController {
         spotLightNode = scene.rootNode.childNode(withName: "spotLight", recursively: true)
         cameraNode = scene.rootNode.childNode(withName: "camera", recursively: true)
         ghost = scene.rootNode.childNode(withName: "ghost reference", recursively: true)
-        ghostScary = scene.rootNode.childNode(withName: "ghost reference", recursively: true)
+        ghostScary = scene.rootNode.childNode(withName: "ghostScary reference", recursively: true)
         ghostScary.isHidden = true
     }
     
     func setupSounds() {
 //        let sawSound = SCNAudioSource(fileNamed: "chainsaw.wav")!
-        let jumpSound = SCNAudioSource(fileNamed: "jump.wav")!
-//        sawSound.load()
-        jumpSound.load()
-//        sawSound.volume = 0.3
-        jumpSound.volume = 0.4
+        let screamSound = SCNAudioSource(fileNamed: "scream.mp3")!
+        screamSound.volume = 20.5
+
+        screamSound.load()
         
-//        sounds["saw"] = sawSound
-        sounds["jump"] = jumpSound
+        sounds["scream"] = screamSound
         
-//        let backgroundMusic = SCNAudioSource(fileNamed: "background.mp3")!
-//        backgroundMusic.volume = 0.1
+//        let backgroundMusic = SCNAudioSource(fileNamed: "BackgroundMusic.wav")!
+//        backgroundMusic.volume = 1.5
 //        backgroundMusic.loops = true
 //        backgroundMusic.load()
-        
+//
 //        let musicPlayer = SCNAudioPlayer(source: backgroundMusic)
-//        ballNode.addAudioPlayer(musicPlayer)
+//        scene.rootNode.addAudioPlayer(musicPlayer)
+
     }
     
     @objc func sceneViewTapped(recognizer:UITapGestureRecognizer){
@@ -141,10 +133,11 @@ class GameViewController: UIViewController {
         if hitResults.count > 0 {
             let result = hitResults.first
             if let node = result?.node{
-                print("\(node.parent?.name)")
-//                print("Ghost \(ghost.parent?.position)")
-                print("Ghost name \(ghost?.name)")
-                print("Physicsc \(ghost?.physicsBody)")
+//                print("\(node.parent?.name)")
+                print("Ghost name \(ghost.parent?.name)")
+                print("Ghost \(ghost.parent?.position)")
+
+//                print("Physicsc \(ghost?.physicsBody)")
 
                 
                 if node.parent?.name == "skin0"{
@@ -152,7 +145,7 @@ class GameViewController: UIViewController {
                     print("Score \(score)")
                     node.parent?.isHidden = true
                     score += 1
-                    scoreLabel.text = "\(score)/10"
+                    scoreLabel.text = "\(score)/5"
                 }
             }
         }
@@ -173,6 +166,14 @@ extension GameViewController: SCNSceneRendererDelegate {
         spotLightNode.position = pov.position
         spotLightNode.orientation = pov.orientation
         spotLightNode.eulerAngles = pov.eulerAngles
+        
+        if score == 5 {
+            print("YOU WIN")
+        }
+        
+        if health <= 0 {
+            print("You LOST")
+        }
     }
 }
 
@@ -198,79 +199,79 @@ extension GameViewController : SCNPhysicsContactDelegate {
             
 //        if(nodeA.childNodes[0].name == "ghost" || nodeB.name == "ghost"){
         if(nodeA.name == "ghost reference" || nodeB.name == "ghost reference"){
-//
-
-            
-
-                print("in")
+            print("in")
             if nodeA.name == "ghost reference"{
                 ghostNode = nodeA.parent
                 }else{
                     ghostNode = nodeB.parent
                 }
             ghostScary.isHidden = true
-            print("Ghost \(ghostNode?.name)")
+//            print("Ghost \(ghostNode?.name)")
             
 //            print("Spotlight \(spotLightNode.position)")
 //            print("Ghost \(ghostNode?.position)")
 //            print("Name \(ghostNode?.name)")
 
-            let waitAction = SCNAction.wait(duration: 5)
+            var x:Float = 0
+            var z:Float = 0
             
             if(ghostNode?.position.z == 4){
-                let unhideAction = SCNAction.run { (node) in
-                    node.isHidden = false
-                }
-                let hidePhysicsc = SCNAction.run { node in
-                    node.childNodes[0].categoryBitMask = 1
-                }
-                
-                let showPhysicsc = SCNAction.run { node in
-                    node.childNodes[0].categoryBitMask = 2
-                }
-                
-                print("ghost node \(ghostNode?.childNodes[0].name)")
-                print("Physicsc \(ghostNode?.childNodes[0].physicsBody)")
-                
-                let waitActionn  = SCNAction.wait(duration: 2)
-                print("jump sound \(sounds["jump"])")
-                let sawSound = sounds["jump"]!
-                ghostNode?.runAction(SCNAction.playAudio(sawSound, waitForCompletion: false))
-                
-                let hideAction = SCNAction.run { (node) in
-                    node.isHidden = true
-                }
-                let ghostScaryAction = SCNAction.sequence([unhideAction, waitActionn, hideAction])
-                
-                let moveAction = SCNAction.move(to: SCNVector3(x: 0, y: 0, z: (ghostNode?.position.z)! * -1), duration: 0)
-                let action = SCNAction.sequence([hidePhysicsc,moveAction, waitAction, showPhysicsc])
-                ghostNode?.runAction(action)
-                ghostScary.runAction(ghostScaryAction)
+                z = -4
+//                let moveAction = SCNAction.move(to: SCNVector3(x: 0, y: 0, z: (ghostNode?.position.z)! * -1), duration: 0)
+//                let action = SCNAction.sequence([hidePhysicsc,moveAction, waitAction, showPhysicsc])
+//                ghostNode?.runAction(action)
 //                print("Ghost hidden \(ghostScary.isHidden)")
-                
-                
 
             }else if ghostNode?.position.z == -4{
-                let moveAction = SCNAction.move(to: SCNVector3(x: -3, y: 0, z: 0), duration: 0)
-                let action = SCNAction.sequence([waitAction,moveAction])
-                ghostNode?.runAction(action)
+                x = -3
+//                let moveAction = SCNAction.move(to: SCNVector3(x: -3, y: 0, z: 0), duration: 0)
+//                let action = SCNAction.sequence([waitAction,moveAction])
+//                ghostNode?.runAction(action)
 
             }else if ghostNode?.position.x == -3{
-                let moveAction = SCNAction.move(to: SCNVector3(x: 5, y: 0, z: 0), duration: 0)
-                let action = SCNAction.sequence([waitAction,moveAction])
-                ghostNode?.runAction(action)
+                x = 5
+//                let moveAction = SCNAction.move(to: SCNVector3(x: 5, y: 0, z: 0), duration: 0)
+//                let action = SCNAction.sequence([waitAction,moveAction])
+//                ghostNode?.runAction(action)
 
             }else if ghostNode?.position.x == 5{
-                let moveAction = SCNAction.move(to: SCNVector3(x: 0, y: 0, z: 4), duration: 0)
-                let action = SCNAction.sequence([waitAction,moveAction])
-                ghostNode?.runAction(action)
+                z = 4
+//                let moveAction = SCNAction.move(to: SCNVector3(x: 0, y: 0, z: 4), duration: 0)
+//                let action = SCNAction.sequence([waitAction,moveAction])
+//                ghostNode?.runAction(action)
             }
-
-//            let sequenceAction = SCNAction.sequence([moveAction, SCNAction.removeFromParentNode()])
-//                    ghostNode?.runAction(sequenceAction)
             
-                }
+            let waitAction = SCNAction.wait(duration: 3)
+            let moveAction = SCNAction.move(to: SCNVector3(x: x, y: 0, z: z), duration: 0)
+            let action = SCNAction.sequence([moveAction])
+            ghostNode?.runAction(action)
+
+            let unhideAction = SCNAction.run { (node) in
+                node.isHidden = false
+            }
+//            let hidePhysicsc = SCNAction.run { node in
+//                node.childNodes[0].categoryBitMask = 1
+//            }
+//            let showPhysicsc = SCNAction.run { node in
+//                node.childNodes[0].categoryBitMask = 2
+//            }
+            
+            let sawSound = sounds["scream"]!
+            sceneView.scene?.rootNode.runAction(SCNAction.playAudio(sawSound, waitForCompletion: false))
+            
+            let hideAction = SCNAction.run { (node) in
+                node.isHidden = true
+            }
+            
+            let ghostScaryAction = SCNAction.sequence([unhideAction, waitAction, hideAction])
+            ghostScary.runAction(ghostScaryAction)
+            
+            health -= 1
         }
+    }
+    
+    
+    
     
 //    func physicsWorld(_ world: SCNPhysicsWorld, didEnd contact: SCNPhysicsContact) {
 //        var ghostNode:SCNNode?
